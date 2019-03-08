1c72ccc6f9524ff9dcf300a97c7ccdd1892efc7d
Jordan Campbell <jordan.campbell01@stclairconnect.ca>
2019-03-08 08:16:58 -0500

    Added google analytics

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'therubyracer', platforms: :ruby
 
 gem 'devise'
+gem 'rack-tracker'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,10 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.middleware.use(Rack::Tracker) do
+    handler :google_analytics, { tracker: 'UA-135901788-1' }
+  end
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
